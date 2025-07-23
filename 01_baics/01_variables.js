299f28996712ce0eefe583aedeeeb59181b0b5a3
[tejas parate] <[tejasparate.st@gmail.com]>
2025-07-23 19:26:02 +0530

    variables

---
+++ 01_baics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 12332
+let accuntEmail = "tejasparate@google.com" 
+var accrountPassword = "12334"
+accountCity = "Pune"
+let accountState ;
+
+// accountId = 2  //const cannot be changed
+
+/* let can be changed */
+accuntEmail = "tp@google.com"
+
+accrountPassword = "2323"
+
+accountCity = "Mumbai"
+
+console.log(accountId);
+console.table([accountId, accuntEmail, accrountPassword, accountCity , accountState]);
+
+
