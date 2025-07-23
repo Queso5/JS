d1907ce2e74a93d86f39ec06e7e3f1797029c74f
[tejas parate] <[tejasparate.st@gmail.com]>
2025-07-23 19:26:31 +0530

    datatypes

---
+++ 01_baics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict" ; //treats js code with newer standards
+
+// alert("hello") //works in browser not in terminal
+
+// number -> integer value
+// string ->  " "
+// BigInt-> 
+// Boolean -> true false
+// null -> standalone value
+// undefined -> no value assigned
+// symbol -> unique value
+// object -> 
+
+//tyepof  -> returns the type of a variable
+
