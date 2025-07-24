75e9f2263d55eb0f2f9eb926034c04cec08fba1b
[tejas parate] <[tejasparate.st@gmail.com]>
2025-07-24 17:02:37 +0530

    type conversions of different data types

---
+++ 01_baics/converstionOperatoins.js
@@ -0,0 +1,29 @@
+let score = "33"
+
+// console.log(typeof score)
+
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+
+
+let value = "22abc"
+// console.log(Number(value))
+//returns Nan(not a number) because cannot convert string to a number
+
+let value2 = null
+// console.log(Number(value2)) //returns 0 because null is converted to 0
+
+let value3 = true
+// console.log(Number(value3)) //returns 1 because true is converted to 1 and false is convrted to 0
+
+//1 -> tur  ; 0 -> false 
+// " " -> false
+// "xyz " -> true
+
+
+
+//convert  integer to string 
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
